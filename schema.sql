5e61fd88849f449065f7128ca12e46651324e20b
Alabi12 <wolo22001@gmail.com>
2023-01-25 16:52:07 +0000

    add and remove tables and data, assigned foreign and primary keys

--- schema.sql
+++ schema.sql
@@ -8,3 +8,30 @@ CREATE TABLE animals(
     neutered BOOLEAN, weight_kg DECIMAL,
     species VARCHAR(255);
     );
+
+/*Create owners table  */
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INT  
+);
+
+/* Create the species table */
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+/* Modify animals table*/
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
