8863cb1c7e01340b4a8533d8d27b9c05580189b5
Zainab-Alzubair <zainab.a.elzubair@gmail.com>
2023-01-30 19:51:15 +0300

    Add email column to owners table

--- schema.sql
+++ schema.sql
@@ -66,6 +66,9 @@ create table visits (animals_id INT, vets_id INT, date_of_visit date);
 ALTER TABLE visits ADD CONSTRAINT animals_fkey FOREIGN KEY (animals_id) REFERENCES animals(id);
 ALTER TABLE visits ADD CONSTRAINT vets_fkey FOREIGN KEY (vets_id) REFERENCES vets(id);
 
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
 ---PERFORMANCE IMPROVEMENT---
 CREATE index animals_id_index ON visits (animals_id);
 CREATE INDEX vets_id_index ON visits (vets_id);
